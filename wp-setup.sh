c6d4558f50495eb0ea87b0c052173f2d5c627c2a
Mark Prout <mark.prout@outlook.com>
2020-12-06 21:06:03 +0000

    Adding wp db connection write

--- wp-setup.sh
+++ wp-setup.sh
@@ -3,9 +3,9 @@ clear
 
 
 # Provide user with the pre-requisites for running this script
-function prerequisites {
-    local ENTRY_VALUE="$1"
-    local NEXT_VALUE="$2"
+prerequisites() {
+    local ENTRY_VALUE=$1
+    local NEXT_VALUE=$2
     echo "-----------------------------------------"
     echo "----------- PRE-REQUISITES --------------"
     echo "-----------------------------------------"
@@ -14,21 +14,21 @@ function prerequisites {
     echo " Granted All Permissions to your MariaDB admin user (not root)"
     echo " --- e.g.  if admin user is myadmin and password is mypassword"
     echo "  GRANT ALL ON *.* TO 'myadmin'@'localhost' IDENTIFIED BY 'mypassword' WITH GRANT OPTION;"
-    read -p "Press enter to continue"
-    return "$NEXT_VALUE"
+    read -p "Press enter to continue" dummy
+    FUNCTION_RESULT=$NEXT_VALUE
 }
 
-function update_apps {
+update_apps() {
     local ENTRY_VALUE="$1"
     local NEXT_VALUE="$2"
     echo "--------------------------------"
     echo "----- Updating App Library -----"
     echo "--------------------------------"
     sudo apt update
-    return "$NEXT_VALUE"
+    FUNCTION_RESULT=$NEXT_VALUE
 }
 
-function apache2_install {
+apache2_install() {
     local ENTRY_VALUE="$1"
     local NEXT_VALUE="$2"
     echo "------------------------------------------"
@@ -42,13 +42,185 @@ function apache2_install {
         echo -e "${GREEN}Already installed!${NC}"
     fi
 
-    return "$NEXT_VALUE"
+    FUNCTION_RESULT=$NEXT_VALUE
 }
 
+php_install() {
+    local ENTRY_VALUE="$1"
+    local NEXT_VALUE="$2"
+    echo "-----  PHP Installation and Setup -----"
+    
+    if [ $(dpkg-query -s -f='$(Status)' php 2>/dev/null | grep -c "ok installed") -eq 0 ];
+    then
+        echo "Installing php + mods"
+        sudo apt -y install php libapache2-mod-php php-mysql
+    else
+        echo -e "${GREEN}php is installed!${NC}"
+    fi
+
+    FUNCTION_RESULT=$NEXT_VALUE
+}
+
+configure_apache_vhost() {
+    local ENTRY_VALUE="$1"
+    local NEXT_VALUE="$2"
+    echo "----- Get Site Information -----"
+    read -p "Domain Name (without www.): " domainname
+
+    echo "----- Create Apache2 vhost if needed and set permissions -----"
+    if [ ! -d "/var/www/$domainname" ];
+    then
+        echo "Apache dir for vhost does not exist... creating it"
+        sudo mkdir /var/www/$domainname
+    fi
+
+    echo " Set vhost directory permissions"
+    sudo chown -R $USER:$USER /var/www/$domainname
+
+    echo "----- Create a new vhost config file, replacing the domains existing one if it exists -----"
+    if [ -f /etc/apache2/sites-available/$domainname.conf ];
+    then
+        echo "Removing file /etc/apache2/sites-available/$domainname.conf"
+        sudo rm -f /etc/apache2/sites-available/$domainname.conf
+    fi
+
+    echo "----- Create vhost file for the new domain -----"
+    sudo cat >/etc/apache2/sites-available/$domainname.conf <<EOF
+    <VirtualHost *:80>
+        ServerName $domainname
+        ServerAlias www.$domainname
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/$domainname
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+        <Directory /var/www/$domainname>
+            AllowOverride All
+        </Directory>
+    </VirtualHost>
+EOF
+
+    echo "----- Apache Enable vhost Site -----"
+    sudo a2ensite $domainname
+
+    echo "----- Disable the default apache site -----"
+    sudo a2dissite 000-default
+
+    echo "----- Enable the apache mod_rewrite mod to allow wordpress permalink -----"
+    sudo a2enmod rewrite
+
+    echo "----- Verify that the vhost configuration is good -----"
+    sudo apache2ctl configtest
+
+    echo "----- Reload the revised apache configurations -----"
+    sudo systemctl reload apache2
+    echo "----- APACHE2 VHOST SETUP END ------------"
+    echo "------------------------------------------"
+
+    FUNCTION_RESULT=$NEXT_VALUE
+}
+
+install_latest_wordpress() {
+    local ENTRY_VALUE="$1"
+    local NEXT_VALUE="$2"
+    echo "---------------------------------------------------------------------"
+    echo "------- DOWNLOAD, READY LATEST WORDPRESS DISTRIBUTION - START -------"
+    echo "        -----------------------------------------------------"
+    cd /tmp
+    curl -O https://wordpress.org/latest.tar.gz
+
+    echo "----- Delete /tmp/wordpress if already exists"
+    if [ -d "/tmp/wordpress" ];
+    then
+        echo "Deleting /tmp/wordpress"
+        sudo rm -rf /tmp/wordpress
+    fi
+
+    echo "----- Unzip latest.tar.gz -----"
+    tar xzvf latest.tar.gz
+    touch /tmp/wordpress/.htaccess
+    cp /tmp/wordpress/wp-config-sample.php /tmp/wordpress/wp-config.php
+    mkdir /tmp/wordpress/wp-content/upgrade
+
+    echo "----- Copy wordpress files into the vhost directory -----"
+    sudo cp -a /tmp/wordpress/. /var/www/$domainname
+
+    echo "----- Change wordpress vhost document directory permissions -----"
+    sudo chown -R www-data:www-data /var/www/$domainname
+    sudo find /var/www/$domainname/ -type d -exec chmod 750 {} \;
+    sudo find /var/www/$domainname/ -type f -exec chmod 640 {} \;
+
+    echo "----- Obtain new secret values needed by Wordpress to be more secure -----"
+    SALT=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
+
+    echo "----- Write salts into vhost wp-config.php -----"
+    STRING='put your unique phrase here'
+    sudo printf '%s\n' "g/$STRING/d" a "$SALT" . w | ed -s /var/www/$domainname/wp-config.php
+
+    echo "------- DOWNLOAD, READY LATEST WORDPRESS DISTRIBUTION - END -------"
+    echo "-------------------------------------------------------------------"
+    FUNCTION_RESULT=$NEXT_VALUE
+}
+
+configure_wordpress_database() {
+    local ENTRY_VALUE="$1"
+    local NEXT_VALUE="$2"
+
+    echo "----------------------------------------------------------"
+    echo "------- Configure MariaDB and Wordpress Connection -------"
+    echo "        ------------------------------------------"
+    echo "Get information for new database (assume it to be on localhost)"
+    echo "Each database will have a seperate db user created utilised by wordpress"
+    read -p "Database Host: " dbhost
+    read -p "Database Admin Username: " dbadmin
+    read -p "Database Admin Password: " dbadminpw
+    read -p "Database Name: " dbname
+    read -p "Database User: " dbuser
+    read -p "Database User Password: " dbpass
+    echo
+    echo "Your inputs were;"
+    echo "Database Host = " $dbhost
+    echo "Database Admin Username: " $dbadmin
+    echo "Database Admin Password: " $dbadminpw
+    echo "Database Name = " $dbname
+    echo "Database User = " $dbuser
+    echo "Database User Password = " $dbpass
+    read -p "Are These Correct, y/n : " dbconfirm
+    if [ $dbconfirm != y ]
+    then
+        echo "exiting"
+        exit
+    fi
+
+    echo "Creating user, database within MariaDB and setting permissions"
+
+    sudo mysql -u $dbadmin -p$dbadminpw <<EOF
+    CREATE USER '$dbuser'@'$dbhost' IDENTIFIED BY '$dbpass';
+    CREATE DATABASE $dbname DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;;
+    GRANT ALL PRIVILEGES ON $dbname.* TO '$dbuser'@'$dbhost';
+    FLUSH PRIVILEGES;
+EOF
+
+    echo "Modify wp-admin.php to include correct database settings : wp-config.php"
+    STRING='database_name_here'
+    sudo printf '%s\n' "g/$STRING/d" a "$dbname" . w | ed -s /var/www/$domainname/wp-config.php
+    STRING='username_here'
+    sudo printf '%s\n' "g/$STRING/d" a "$dbuser" . w | ed -s /var/www/$domainname/wp-config.php
+    STRING='password_here'
+    sudo printf '%s\n' "g/$STRING/d" a "$dbpass" . w | ed -s /var/www/$domainname/wp-config.php
+    STRING='localhost'
+    sudo printf '%s\n' "g/$STRING/d" a "$dbhost" . w | ed -s /var/www/$domainname/wp-config.php
+
+    if [ ! grep -Fxq "define( 'FS_METHOD', 'direct' );" /var/www/$domainname/wp-config.php ];
+    then
+        sudo cat "define( 'FS_METHOD', 'direct' );" >> /var/www/$domainname/wp-config.php
+    fi
+    
+    FUNCTION_RESULT=$NEXT_VALUE
+}
 
 # If state.mc file does not exist create and load with initial value of 0
 STATE_FILE="/home/ubuntu/state.mc"
-ENTRY_STATE="0"
+ENTRY_STATE=1
 if [ ! -f $STATE_FILE ];
 then
     echo " Initialising state file : $STATE_FILE"
@@ -62,7 +234,7 @@ echo " State File Content: $ENTRY_STATE"
 
 # Run the state machine, exit if the state entry is equal to existing state
 # or 99 which represents completion.
-EXIT_FLAG="0"
+EXIT_FLAG=0
 until [ $EXIT_FLAG = 1 ]; do
     echo "EXIT_FLAG: $EXIT_FLAG"
     if [ $EXIT_FLAG = 0 ];
@@ -71,20 +243,36 @@ until [ $EXIT_FLAG = 1 ]; do
         echo "ENTRY_STATE: $ENTRY_STATE"
         NEW_STATE="$ENTRY_STATE"
         case $ENTRY_STATE in
-            "0")
-                prerequisites "0" "1"
-                NEW_STATE=$?
+            1)
+                prerequisites 1 2
+                NEW_STATE=$FUNCTION_RESULT
                 ;;
-            "1")
-                update_apps "1" "2"
-                NEW_STATE=$?
+            2)
+                update_apps 2 3
+                NEW_STATE=$FUNCTION_RESULT
                 ;;
-            "2")
-                apache2_install "2" "3"
-                NEW_STATE=$?
+            3)
+                apache2_install 3 4
+                NEW_STATE=$FUNCTION_RESULT
+                ;;
+            4)
+                php_install 4 5
+                NEW_STATE=$FUNCTION_RESULT
+                ;;
+            5)
+                configure_apache_vhost 5 6
+                NEW_STATE=$FUNCTION_RESULT
+                ;;
+            6)
+                install_latest_wordpress 6 7
+                NEW_STATE=$FUNCTION_RESULT
+                ;;
+            7)
+                configure_wordpress_database 7 8
+                NEW_STATE=$FUNCTION_RESULT
                 ;;
             *)
-                NEW_STATE="99"
+                NEW_STATE=99
                 ;;
         esac
 
@@ -104,142 +292,3 @@ until [ $EXIT_FLAG = 1 ]; do
 done
 
 exit
-
-
-
-
-
-echo "-----  PHP Installation and Setup -----"
-if [ $(dpkg-query -s -f='$(Status)' php 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-    echo "Installing php + mods"
-    sudo apt -y install php libapache2-mod-php php-mysql
-else
-    echo -e "${GREEN}php is installed!${NC}"
-fi
-
-
-
-echo "----- Get Site Information -----"
-read -p "Domain Name (without www.): " domainname
-
-echo "----- Create Apache2 vhost if needed and set permissions -----"
-if [ ! -d "/var/www/$domainname" ];
-then
-    echo "Apache dir for vhost does not exist... creating it"
-    sudo mkdir /var/www/$domainname
-fi
-
-echo " Set vhost directory permissions"
-sudo chown -R $USER:$USER /var/www/$domainname
-
-echo "----- Create a new vhost config file, replacing the domains existing one if it exists -----"
-if [ -f /etc/apache2/sites-available/$domainname.conf ];
-then
-    echo "Removing file /etc/apache2/sites-available/$domainname.conf"
-    sudo rm -f /etc/apache2/sites-available/$domainname.conf
-fi
-
-echo "----- Create vhost file for the new domain -----"
-sudo sh -c "cat >/etc/apache2/sites-available/$domainname.conf <<EOF
-<VirtualHost *:80>
-    ServerName $domainname
-    ServerAlias www.$domainname
-    ServerAdmin webmaster@localhost
-    DocumentRoot /var/www/$domainname
-    ErrorLog ${APACHE_LOG_DIR}/error.log
-    CustomLog ${APACHE_LOG_DIR}/access.log combined
-    <Directory /var/www/$domainname >
-        AllowOverride All
-    </Directory>
-</VirtualHost>
-EOF"
-
-echo "----- Apache Enable vhost Site -----"
-sudo a2ensite $domainname
-
-echo "----- Disable the default apache site -----"
-sudo a2dissite 000-default
-
-echo "----- Enable the apache mod_rewrite mod to allow wordpress permalink -----"
-sudo a2enmod rewrite
-
-echo "----- Verify that the vhost configuration is good -----"
-sudo apache2ctl configtest
-
-echo "----- Reload the revised apache configurations -----"
-sudo systemctl reload apache2
-echo "----- APACHE2 VHOST SETUP END ------------"
-echo "------------------------------------------"
-
-read -p "Continue " dummy
-
-echo "---------------------------------------------------------------------"
-echo "------- DOWNLOAD, READY LATEST WORDPRESS DISTRIBUTION - START -------"
-echo "        -----------------------------------------------------"
-cd /tmp
-curl -O https://wordpress.org/latest.tar.gz
-
-echo "----- Delete /tmp/wordpress if already exists"
-if [ -d "/tmp/wordpress" ];
-then
-    echo "Deleting /tmp/wordpress"
-    sudo rm -rf /tmp/wordpress
-fi
-
-echo "----- Unzip latest.tar.gz -----"
-tar xzvf latest.tar.gz
-touch /tmp/wordpress/.htaccess
-cp /tmp/wordpress/wp-config-sample.php /tmp/wordpress/wp-config.php
-mkdir /tmp/wordpress/wp-content/upgrade
-
-echo "----- Copy wordpress files into the vhost directory -----"
-sudo cp -a /tmp/wordpress/. /var/www/$domainname
-
-echo "----- Change wordpress vhost document directory permissions -----"
-sudo chown -R www-data:www-data /var/www/$domainname
-sudo find /var/www/$domainname/ -type d -exec chmod 750 {} \;
-sudo find /var/www/$domainname/ -type f -exec chmod 640 {} \;
-
-echo "----- Obtain new secret values needed by Wordpress to be more secure -----"
-SALT=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
-
-echo "----- Write salts into vhost wp-config.php -----"
-STRING='put your unique phrase here'
-sudo printf '%s\n' "g/$STRING/d" a "$SALT" . w | ed -s wp-config.php
-
-echo "------- DOWNLOAD, READY LATEST WORDPRESS DISTRIBUTION - END -------"
-echo "-------------------------------------------------------------------"
-
-
-echo "----------------------------------------------------------"
-echo "------- Configure MariaDB and Wordpress Connection -------"
-echo "        ------------------------------------------"
-echo "Get information for new database (assume it to be on localhost)"
-read -p "Database Name: " dbname
-read -p "Database User: " dbuser
-read -p "Database Password: " dbpass
-echo
-echo "Your inputs were;"
-echo "Database Name = " $dbname
-echo "Databse User = " $dbuser
-echo "Database Password = " $dbpass
-read -p "Are These Correct, y/n : " dbconfirm
-if [ $dbconfirm != y ]
-then
-    echo "exiting"
-    exit
-fi
-
-echo "Creating user, database within MariaDB and setting permissions"
-sudo mysql -u root -p <<EOF
-CREATE USER '$dbuser'@'localhost' IDENTIFIED BY '$dbpass';
-CREATE DATABASE $dbname DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;;
-GRANT ALL PRIVILEGES ON $dbname.* TO '$dbuser'@'localhost';
-FLUSH PRIVILEGES;
-EOF
-
-echo "Modify wp-admin.php to include correct database settings"
-
-
-echo
